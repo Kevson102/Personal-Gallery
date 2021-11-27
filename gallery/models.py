8a31adf6f5e9555fbff94dae8a5c4cabf5ac986a
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-27 14:55:44 +0300

    CREATES A TEST FOR THE CATEGORY SAVE METHOD AND WRITES CODE IN THE CLASS CATEGORY TO MAKE THE TEST PASS

--- gallery/models.py
+++ gallery/models.py
@@ -9,10 +9,20 @@ class Location(models.Model):
 class Category(models.Model):
   category = models.CharField(max_length= 15)
   
+  def __str__(self):
+    return self.category
+  
+  # Save method
+  def save_category(self):
+    self.save()
+  
 class Image(models.Model):
   image = models.ImageField(upload_to = 'static/images/', blank=True)
   image_name = models.CharField(max_length=30)
   image_description = models.TextField()
   location = models.ForeignKey(Location, on_delete=models.CASCADE)
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.image_name
   
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+class CategoryTestCase(TestCase):
+  # Set up method
+  def setUp(self):
+    self.new_category = Category(category = 'Travel')
+    
+  # Test instace
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_category, Category))
+    
+  # Test Save category method
+  def test_save_method(self):
+    self.new_category.save_category()
+    categories = Category.objects.all()
+    self.assertTrue(len(categories)==1)
