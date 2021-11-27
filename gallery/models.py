918dc0e84d6267fbf8564866633bb3cc9cb8250d
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-27 14:16:22 +0300

    CREATES THE LOCATION MODEL, THE CATEGORY MODEL, AND THE IMAGE MODEL AND MAKES A MIGRATION

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+  location_name = models.CharField(max_length=30)
+  country = models.CharField(max_length=20)
+  region = models.CharField(max_length=20)
+  
+class Category(models.Model):
+  category = models.CharField(max_length= 15)
+  
+class Image(models.Model):
+  image = models.ImageField(upload_to = 'static/images/', blank=True)
+  image_name = models.CharField(max_length=30)
+  image_description = models.TextField()
+  location = models.ForeignKey(Location, on_delete=models.CASCADE)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  
