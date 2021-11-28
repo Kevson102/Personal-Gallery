1620b0d42208aa86671d318c61cfae0e1f5bd9ed
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-28 10:10:58 +0300

    ADDS THE FUNCTIONALITY OF DISPLAYING A LARGER AND DETAILED VERSION OF THE IMAGES ON CLICK

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$', views.home, name='home')
+    url('^$', views.home, name='home'),
+    url(r'^details/(?P<photo_id>\d+)', views.image_details, name='image_details'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Image, Location, Category
 
 # Create your views here.
 def home(request):
   images = Image.get_all_images()
   return render(request, 'index.html',{"images":images})
+
+# Display the details of a photo
+def image_details(request, photo_id):
+  try:
+    pic_details = Image.objects.get(pk = photo_id)
+  except DoesNotExist:
+    raise Http404()
+  return render(request, "detailedPic.html", {"details":pic_details})
